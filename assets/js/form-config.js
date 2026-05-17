0bb58734974bce17d9a2d0a4cb36ff8066b5ee37
takeshimonoseki <takeshimonoseki@gmail.com>
2026-05-17 19:49:27 +0900

    chore: set production form webapp url

--- assets/js/form-config.js
+++ assets/js/form-config.js
@@ -1,3 +1,3 @@
 window.KAZOKU_FORM_CONFIG = {
-  gasWebAppUrl: "",
+  gasWebAppUrl: "https://script.google.com/macros/s/AKfycbxMEqGChrE_VgQuSTBvEjcW_0DUj6SC6mKPTEhl_58hR9zusJXGmCgL5LxnBnKnuf3j_g/exec",
 };
